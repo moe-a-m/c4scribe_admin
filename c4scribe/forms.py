5506cae71fb468ead06c1b4f01ff35ac156819ef
melhassan10 <melhassan479@gmail.com>
2024-05-02 16:33:18 -0400

    minor modifications

--- c4scribe/forms.py
+++ c4scribe/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class PromptForm(forms.Form):
-    prompt = forms.CharField(label='Input Text',
+    prompt = forms.CharField(label='write the software description',
                             widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 10}))
